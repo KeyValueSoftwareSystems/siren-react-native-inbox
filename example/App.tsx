49955bfbabcde8ce9af93bdabdbb2ca1d8622bad
Abhay <abhay@keyvalue.systems>
2024-03-14 15:49:21 +0530

    fix: update app tsx

--- example/App.tsx
+++ example/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { SirenProvider } from '@siren/react-native-inbox';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -28,15 +28,6 @@ function App(): React.JSX.Element {
     userToken: 'fb928b226c5b47b7810171acbe2dbad2',
     recipientId: '6b6027be-7882-4eca-9cc7-080a06798c71'
   });
-
-  useEffect(() => {
-    setTimeout(() => {
-      setConfig({
-        userToken: '227047349b3044fbac7c24fdf5537c44',
-        recipientId: '551cd601-65b3-41b2-bbce-bbd15908992f'
-      })
-    }, 3000);
-  }, []);
 
   return (
     <NavigationContainer>
