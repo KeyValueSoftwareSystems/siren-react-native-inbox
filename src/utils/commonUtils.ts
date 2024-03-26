15843e0d7bd0a2ca16135abb986ddff871a91e21
Abhay Keyvalue <95466701+abhay-keyvalue@users.noreply.github.com>
2024-03-26 11:27:53 +0530

    fix: Add accurate month logic to generateElapsedTimeText function (#29)

--- src/utils/commonUtils.ts
+++ src/utils/commonUtils.ts
@@ -63,12 +63,14 @@ export const generateElapsedTimeText = (timeString: string) => {
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
   const years = Math.floor(days / 365);
 
   if (millisecondsDiff < 60000) return 'Just now';
   else if (minutes < 60) return minutes === 1 ? '1 minute ago' : `${minutes} minutes ago`;
   else if (hours < 24) return hours === 1 ? '1 hour ago' : `${hours} hours ago`;
-  else if (days < 365) return days === 1 ? '1 day ago' : `${days} days ago`;
+  else if (days < 30) return days === 1 ? '1 day ago' : `${days} days ago`;
+  else if (months < 12) return months === 1 ? '1 month ago' : `${months} months ago`;
   else return years === 1 ? '1 year ago' : `${years} years ago`;
 };
 
