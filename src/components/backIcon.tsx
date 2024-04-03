65939b79f8a6d508049a38f2e4d46539e8f551ff
Abhay <abhay@keyvalue.systems>
2024-04-03 11:13:17 +0530

    fix: Update back icon style

--- src/components/backIcon.tsx
+++ src/components/backIcon.tsx
@@ -20,20 +20,20 @@ const style = StyleSheet.create({
     width: 20,
   },
   backIconLine1: {
-    width: 15,
+    width: 12,
     height: 2,
     backgroundColor: 'red',
     transform: [{
-      rotate: '125deg'
+      rotate: '130deg'
     }]
   },
   backIconLine2: {
-    marginTop: 10,
-    width: 15,
+    marginTop: 6,
+    width: 12,
     height: 2,
     backgroundColor: 'red',
     transform: [{
-      rotate: '235deg'
+      rotate: '230deg'
     }]
   }
 });
